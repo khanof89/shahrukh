e1d92690c36aabdc38f3fb3769a28d26383b922d
shahrukh <khan_shahrukh89@yahoo.com>
2016-10-03 16:15:24 +0530

    changed the title of blogs

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -7,25 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 
     <!-- Title -->
-    <title>Mi. - Ultimate Personal Resume vCard Template</title>
+    <title>I am Shahrukh - Blog</title>
 
     <!-- Favicons -->
-    <link rel="shortcut icon" href="assets/img/favicon.png">
-    <link rel="apple-touch-icon" href="assets/img/favicon_60x60.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicon_76x76.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicon_120x120.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicon_152x152.png">
+    <link rel="shortcut icon" href="/assets/img/favicon.png">
+    <link rel="apple-touch-icon" href="/assets/img/favicon_60x60.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/favicon_76x76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/assets/img/favicon_120x120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/img/favicon_152x152.png">
 
     <!-- Google Web Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,500,300,100' rel='stylesheet' type='text/css'>
 
     <!-- CSS Styles -->
-    <link rel="stylesheet" href="assets/css/styles.css" />
+    <link rel="stylesheet" href="/assets/css/styles.css" />
 
     <!-- CSS Template -->
-    <link rel="stylesheet" href="assets/css/theme.min.css" />
-    <link rel="stylesheet" href="assets/css/color/blue-beige.css" id="color" />
+    <link rel="stylesheet" href="/assets/css/theme.min.css" />
+    <link rel="stylesheet" href="/assets/css/color/blue-beige.css" id="color" />
 
 </head>
 
@@ -74,13 +74,6 @@
             <!-- Pagination -->
             <nav class="text-center">
                 {!! $results->render() !!}
-                {{--<ul class="pagination">
-                    <li><a href="#" aria-label="Previous"><i class="ti-arrow-left"></i></a></li>
-                    <li class="active"><a href="#">1</a></li>
-                    <li><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                    <li><a href="#" aria-label="Next"><i class="ti-arrow-right"></i></a></li>
-                </ul>--}}
             </nav>
 
         </div>
@@ -99,7 +92,7 @@
             <!-- Close -->
             <a href="#" class="messenger-close icon icon-hover icon-xs icon-circle icon-white" data-target="messenger"><i class="fa fa-times"></i></a>
             <!-- Avatar -->
-            <img src="assets/img/photos/it-avatar-96x96.jpg" alt="..." class="img-circle margin-b-30">
+            <img src="/assets/img/photos/it-avatar-96x96.jpg" alt="..." class="img-circle margin-b-30">
             <h3>Please fill the <strong>form</strong> - I will response as fast as I can!</h3>
             <!-- Contact Form -->
             <form id="contact-form">
@@ -158,19 +151,16 @@
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 
 <!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information. -->
-<!--
 <script>
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-XXXXX-X']);
-    _gaq.push(['_trackPageview']);
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+  ga('create', 'UA-85128507-1', 'auto');
+  ga('send', 'pageview');
+
 </script>
--->
 
 </body>
 
