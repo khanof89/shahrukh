c8475bd43d2675fe86590ff90d29269929d6c449
shahrukh <khan_shahrukh89@yahoo.com>
2016-10-12 15:44:19 +0530

    added pushwoosh

--- resources/views/push.blade.php
+++ resources/views/push.blade.php
@@ -4,7 +4,33 @@
         Testing Push Notification
     </title>
 
+    <link rel="manifest" href="/manifest.json">
+    <script type="text/javascript" src="//cdn.pushwoosh.com/webpush/pushwoosh-web-notifications.js" async></script>
     <script type="text/javascript">
+        var Pushwoosh = Pushwoosh || [];
+        Pushwoosh.push(['init', {
+            logLevel: 'info', // or debug
+            applicationCode: '5FB14-BB89E',
+            defaultNotificationTitle: 'I am Shahrukh',
+            defaultNotificationImage: 'https://cp.pushwoosh.com/img/logo-medium.png'
+        }]);
+    </script>
+
+
+    <script language="text/javascript">
+        Pushwoosh.push(function(api) {
+            api.getTags().then(function(result) {
+                if (!result.result['My Tag']) {
+                    return api.setTags({'My Tag': 'value'}).then(function(r) {
+                        console.log('setTags result', r);
+                    });
+                }
+            }).catch(function(e) {
+                console.log('error', e);
+            });
+        })
+    </script>
+    {{--<script type="text/javascript">
         (function(p,u,s,h){
             p._pcq=p._pcq||[];
             p._pcq.push(['_currentTime',Date.now()]);
@@ -15,6 +41,6 @@
             h=u.getElementsByTagName('script')[0];
             h.parentNode.insertBefore(s,h);
         })(window,document);
-    </script>
+    </script>--}}
 </head>
 </html>
